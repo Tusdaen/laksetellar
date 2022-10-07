2df8c824bf3218ff6ef936a6745343d2ab227d79
Tusda <e-marwib@asvg.no>
2022-10-07 09:01:14 +0200

    First version laksetellar

---
+++ laksetellar.py
@@ -0,0 +1,12 @@
+
+def function(laks):
+    for i in range(laks):
+        if i+1 == 1:
+            print(i+1,"laks")
+        else:
+            print(i+1,"laksar")
+
+
+while True:
+    antallaks = int(input("Hvor mange laks er det"))
+    function(antallaks)
